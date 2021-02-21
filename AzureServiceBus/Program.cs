386bdfa49b06b2deabcd7f6d492e7e8673596fb2
Purunep <purunep@live.com>
2021-02-20 21:44:24 -0500

    All changes

---
+++ AzureServiceBus/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using Microsoft.Azure.ServiceBus;
+
+namespace AzureServiceBus
+{
+    class Program
+    {
+        static string connectionString = "Endpoint=sb://purunep.servicebus.windows.net/;SharedAccessKeyName=mypolicy;SharedAccessKey=KXLmjs9x7FobAgjnWaXUq0yFa+0SxtZb34iGUBjhuHA=;";
+        static string quepath = "demoqueue";
+        static void Main(string[] args)
+        {
+            var queueClient = new QueueClient(connectionString, quepath);
+            for(int i=0;i<20;i++)
+            {
+                var data = $"Data: {i}";
+                var message = new Message(Encoding.UTF8.GetBytes(data));
+                queueClient.SendAsync(message).Wait();
+                Console.WriteLine($"Data sent: {i}");
+            }
+            queueClient.CloseAsync().Wait();
+            Console.WriteLine("Message sent");
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ AzureServiceBusReceiver/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace AzureServiceBusReceiver
+{
+    class Program
+    {
+        static string connectionString = "Endpoint=sb://purunep.servicebus.windows.net/;SharedAccessKeyName=mypolicy;SharedAccessKey=KXLmjs9x7FobAgjnWaXUq0yFa+0SxtZb34iGUBjhuHA=;";
+        static string quepath = "demoqueue";
+        static void Main(string[] args)
+        {
+
+            var queueClient = new QueueClient(connectionString, quepath);
+
+            queueClient.RegisterMessageHandler(ProcessMessagesAsync, HandleErrorMessageAsync);
+
+            Console.WriteLine("Press enter to exit"); // we need this because message will be receiving in a different thread
+            Console.ReadLine();
+            queueClient.CloseAsync().Wait();
+
+
+        }
+
+        private static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            var content = Encoding.UTF8.GetString(message.Body);
+            Console.WriteLine($"Received Message:{content}");
+        }
+
+        private static Task HandleErrorMessageAsync(ExceptionReceivedEventArgs arg)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+    }
+}
